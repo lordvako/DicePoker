84374bb91cf74d0fc76169ae459231ae7f59660e
lordvako <91406565+lordvako@users.noreply.github.com>
2026-07-31 14:56:17 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.dicepoker"
+        applicationId = "lordvako.appname"
         minSdk = 24
         targetSdk = 35
         versionCode = 1
